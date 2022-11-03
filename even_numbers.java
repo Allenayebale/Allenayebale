0daf30cf2a85010335bdef3ceb2018575a210515
Allenayebale <117280288+Allenayebale@users.noreply.github.com>
2022-11-03 04:49:50 -0700

    Add files via upload
    This is my course work JAVA program

---
+++ even_numbers.java
@@ -0,0 +1,15 @@
+package motivate;
+
+public class even_numbers {
+
+	public static void main(String[] args) {
+		int number=516;
+		System.out.println("The numbers are: ");
+		for(int x=213; x<number; x++) {
+		
+if(x%2==0) {
+	System.out.println(x + "");   }
+}
+	}
+
+}
